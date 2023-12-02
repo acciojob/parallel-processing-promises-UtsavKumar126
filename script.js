0f46413b6ae4bc3c51258003cdd01254fe3da4c6
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-12-02 16:58:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+async function fetch(url){
+	console.log(fetch("https://picsum.photos/id/237/200/300" ))
+}
